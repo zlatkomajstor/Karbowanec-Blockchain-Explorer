a058a9f9a0bf680526d125d67182a9df51245f42
zlatkomajstor <33296664+zlatkomajstor@users.noreply.github.com>
2018-01-01 16:51:14 +0100

    Initial changes

--- q/hashrate/index.php
+++ q/hashrate/index.php
@@ -2,12 +2,12 @@
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_URL, 'http://52.21.253.162:32348/getinfo');
+curl_setopt($ch, CURLOPT_URL, 'http://us5.balkancoin.org:38777/getinfo');
 $result = curl_exec($ch);
 $obj = json_decode($result, TRUE);
 curl_close($ch);
 //print_r($obj);
 $difficulty = $obj['difficulty'];
-$hashrate = round($difficulty / 240);
+$hashrate = round($difficulty / 30);
 print_r($hashrate);
 ?>
