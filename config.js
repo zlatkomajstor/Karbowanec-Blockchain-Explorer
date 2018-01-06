08a8ff0cfd5ebb8a231af4b34644c2b6039f72ef
zlatkomajstor <33296664+zlatkomajstor@users.noreply.github.com>
2018-01-06 21:22:45 +0100

    Playing with colors

--- config.js
+++ config.js
@@ -6,11 +6,11 @@ var refreshDelay = 30000;
 // pools stats by MainCoins
 var networkStat = {
     "bkc": [
-		["us2.mininggood.com", "http://us2.mininggood.com:8147"]		
+		["bkc.mininggood.com", "http://us2.mininggood.com:8147"]		
     ]
 };
 
 var networkStat2 = {
-    "krb": [
+    "bkc": [
 		]
 };
