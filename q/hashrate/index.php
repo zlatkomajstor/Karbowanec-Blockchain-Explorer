ea64c804b356dd0687aaff81110ee89117ebd294
zlatkomajstor <33296664+zlatkomajstor@users.noreply.github.com>
2018-01-14 16:35:38 +0100

    URL change

--- q/hashrate/index.php
+++ q/hashrate/index.php
@@ -2,7 +2,7 @@
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_URL, 'http://us5.balkancoin.org:38777/getinfo');
+curl_setopt($ch, CURLOPT_URL, 'http://explorer.balkancoin.org:38777/getinfo');
 $result = curl_exec($ch);
 $obj = json_decode($result, TRUE);
 curl_close($ch);
--- q/reward/index.php
+++ q/reward/index.php
@@ -1,6 +1,6 @@
 <?php 
 $data_string = '{"jsonrpc":"2.0","id":"test","method":"getlastblockheader","params":" "}';
-$ch = curl_init('http://us5.balkancoin.org:38777/json_rpc');
+$ch = curl_init('http://explorer.balkancoin.org:38777/json_rpc');
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
