218ee88408a500dcf142bbefeb92480d7b2f1084
zlatkomajstor <33296664+zlatkomajstor@users.noreply.github.com>
2019-04-11 22:51:16 +0200

    Use curl instead file_get_contents

--- q/util.php
+++ q/util.php
@@ -18,13 +18,26 @@ function build_rpc_body($method, $params) {
 function fetch_rpc($api, $method, $params) {
 	$url = $api . '/json_rpc';
 	$rendered_rpc = build_rpc_body($method, $params);
-	$context = build_post_context($rendered_rpc);
-	$response = file_get_contents($url, false, $context);
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $rendered_rpc);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+	    'Content-Type: application/json',
+	    'Content-Length: ' . strlen($rendered_rpc))
+	);
+	$response = curl_exec($ch);
+	curl_close($ch);
 	return json_decode($response, true);
 };
 
 function fetch_getinfo($api) {
 	$_url = $api . '/getinfo';
-	$response = file_get_contents($_url);
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_URL, $_url);
+	$response = curl_exec($ch);
+	curl_close($ch);
 	return json_decode($response, true);
 };
